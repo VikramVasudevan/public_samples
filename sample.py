0976c34619a89cdaaa612079f8ad2280b1552a4d
Vikram Vasudevan <vasudevan.vikram@gmail.com>
2022-04-14 16:41:30 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,8 +1,8 @@
 import random
 
-def helloWorld():
+def helloWorld(arg0):
   return [ {"id" : 1, "name" : "Vikram"},{"id" : 2, "name" : "Vasudevan"},{"id" : 3, "name" : "Srinivas"},{"id" : 4, "name" : "JG"},{"id" : 5, "name" : "Mahesh"},{"id" : 6, "name" : "Prajosh"},{"id" : 7, "name" : "Syamu"} ];
 
-def randomNumbers():
+def randomNumbers(arg0):
   return [{"id" : 1, "random" : random.random()},{"id" : 2, "random" : random.random()},{"id" : 3, "random" : random.random()},{"id" : 4, "random" : random.random()},{"id" : 5, "random" : random.random()},{"id" : 6, "random" : random.random()},{"id" : 7, "random" : random.random()}]
   
