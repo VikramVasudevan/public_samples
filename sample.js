10963b7035e3eca79be61bc835149d7612cf9a6d
Vikram Vasudevan <vasudevan.vikram@gmail.com>
2022-04-14 10:37:04 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,3 +1,3 @@
-function helloWorld(){ return JSON.stringify(['Hello World','How are you?']); } 
+function helloWorld(){ return JSON.stringify([{ 'message' : 'Hello World' } , {'message' : 'How are you?'}]); } 
 
-function generateNumbers(){ return JSON.stringify([ { 'random_number' : Math.random(), 'some_message':'How are you?'} ]); }
+function generateNumbers(){ return JSON.stringify([ { 'random_number' : Math.random(), 'message':'How are you?'} ]); }
