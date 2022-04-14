7012a7174be61d8e4e23decd4f752371d54438a6
Vikram Vasudevan <vasudevan.vikram@gmail.com>
2022-04-14 13:14:37 +0530

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,2 @@
+def helloWorld:
+  return [ {"id" : 1, "name" : "Vikram"},{"id" : 2, "name" : "Vasudevan"},{"id" : 3, "name" : "Srinivas"},{"id" : 4, "name" : "JG"},{"id" : 5, "name" : "Mahesh"},{"id" : 6, "name" : "Prajosh"},{"id" : 7, "name" : "Syamu"} ];
